44f49f7d20200c1df3fb22bf7e9b228c7029496c
Angelcortes11 <164367269+Angelcortes11@users.noreply.github.com>
2024-10-23 10:46:41 -0300

    Add files via upload

--- SistemaEscolar/ClsEstudiante.py
+++ SistemaEscolar/ClsEstudiante.py
@@ -2,30 +2,30 @@ import sqlite3
 
 class Estudiante:
 
-    def _init_(self,nombre, edad, anho_id):
-    self.nombre = nombre
-    self.edad = edad
-    self.anho_id = anho_id
+    def _init_(self, nombre, edad, anho_id):
+        self.nombre = nombre
+        self.edad = edad
+        self.anho_id = anho_id
 
-def guardar(self):
+    def guardar(self):
 
-    conn = sqlite3.connect('escolar.db')
-    c = conn.cursor()
-
-    c.execute('INSERT INTO Estudiantes (nombre, edad, anho_id) VALUES (?, ?, ?)',
-        (self, nombre, self.edad, self.anho_id))
-
-    conn.commit()
-    conn.close()
-
-    @staticmethod
-    def obtener_estudiantes():
         conn = sqlite3.connect('escolar.db')
         c = conn.cursor()
 
-        c.execute('SELECT * FROM Estudiantes')
+        c.execute('INSERT INTO Estudiantes (nombre, edad, anho_id) VALUES (?, ?, ?)',
+            (self.nombre, self.edad, self.anho_id))
 
-        estudiantes = c.fetchall()
+        conn.commit()
         conn.close()
 
-        return estudiantes 
+        @staticmethod
+        def obtener_estudiantes():
+            conn = sqlite3.connect('escolar.db')
+            c = conn.cursor()
+
+            c.execute('SELECT * FROM Estudiantes')
+
+            estudiantes = c.fetchall()
+            conn.close()
+
+            return estudiantes 
